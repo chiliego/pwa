14efbe3e2f577187c4fd56db3acc611d3c703f6a
chiliego <chi.vuhuu@gmail.com>
2018-09-20 16:14:23 +0200

    adding room now updates list and rerender page

--- src/components/my-xiaomi.js
+++ src/components/my-xiaomi.js
@@ -1,4 +1,5 @@
 import { html } from '@polymer/lit-element';
+import { repeat } from 'lit-html/lib/repeat.js';
 import '@polymer/iron-icon/iron-icon.js';
 import '@polymer/iron-icons/iron-icons.js';
 import '@polymer/paper-input/paper-input.js';
@@ -9,56 +10,65 @@ import { PageViewElement } from './page-view-element.js';
 import { SharedStyles } from './shared-styles.js';
 
 class MyXiaomi extends PageViewElement {
-  _render(props) {
-    return html`
+  _render({rooms}) {
+      console.log("_render");
+      let newRoomInput = {};
+      return html`
       ${SharedStyles}
+      <style>
+        section > paper-button {
+            display: block;
+            max-width: 15%;
+            text-align: center;
+        }
+      
+      </style>
       <section>
         <h2>Xiaomi Robot</h2>
-        
-        
-        
-        <paper-input id="room" class="search" label="Raum" on-change="${i => props._newRoomNameInput = i.target}">
+        <paper-input id="room" class="search" label="Raum" on-change="${i => newRoomInput.name = i.target}">
         </paper-input>
-        <paper-input id="coordinates" class="search" label="Koordinaten" on-change="${i => props._newRoomCoordsInput = i.target}">
+        <paper-input id="coordinates" class="search" label="Koordinaten" on-change="${i => newRoomInput.coords = i.target}">
         </paper-input>
-       <paper-button raised class="indigo" on-click="${() => this.addRoom()}">Hinzufügen</paper-button>
-       <!--<button on-click="${() => this.addRoom()}">Neuer Raum</button>-->
+       <paper-button raised class="indigo" on-click="${() => this.addRoom(newRoomInput)}">Hinzufügen</paper-button>
+      </section>
+      <section>
+        <ul>
+            ${repeat(rooms, (room) => html`
+                <li>${room.name}: ${room.coords}</li>
+            `)}
+        </ul> 
       </section>
     `
   }
 
   constructor() {
     super();
-    this.rooms = [];
+    this.rooms = [{name: "Küche", coords: "[[25600,25600,25600,25600,1]]"}];
   }
 
   static get properties() { return {
     rooms: Array,
-    _newRoomName: String,
-    _newRoomCoords: String,
-    _newRoomNameInput: Object,
-    _newRoomCoordsInput: Object
-
   }}
 
-  addRoom() {
-    if(this._newRoomNameInput === undefined){
+  addRoom(newRoomInput) {
+    if(newRoomInput.name === undefined){
       console.log("No room Name!");
       return;
     }
 
-    if(this._newRoomNameInput === undefined){
+    if(newRoomInput.coords === undefined){
       console.log("No coordinates!");
       return;
     }
 
-    console.log("Room: " + this._newRoomNameInput.value);
-    console.log("Coords: " + this._newRoomCoordsInput.value);
+    console.log("Room: " + newRoomInput.name.value);
+    console.log("Coords: " + newRoomInput.coords.value);
+    let newRoom = {name: newRoomInput.name.value, coords: newRoomInput.coords.value}
 
-    this.rooms.push({name: this._newRoomNameInput.value, coords: this._newRoomCoordsInput.value})
+    this.rooms = [...this.rooms, newRoom];
 
-    this._newRoomNameInput.value = "";
-    this._newRoomCoordsInput.value = "";
+    newRoomInput.name.value = "";
+    newRoomInput.coords.value = "";
   }
 }
 
